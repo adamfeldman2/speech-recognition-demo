4b7a1239741671c0d6fdda584452cf0410094a8a
Adam Feldman <feldman238@gmail.com>
2017-10-19 12:45:47 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+var colorText = document.querySelector('.screen-color');
+var startListeningButton = document.querySelector('.start-listening');
+var colors = [
+  'aqua',
+  'beige',
+  'black',
+  'blue',
+  'brown',
+  'chocolate',
+  'coral',
+  'fuchsia',
+  'gold',
+  'gray',
+  'grey',
+  'green',
+  'indigo',
+  'ivory',
+  'lavender',
+  'magenta',
+  'maroon',
+  'navy',
+  'olive',
+  'orange',
+  'pink',
+  'purple',
+  'red',
+  'silver',
+  'teal',
+  'turquoise',
+  'violet',
+  'white',
+  'yellow'
+];
+
+var recognition = new (window.SpeechRecognition ||
+  window.webkitSpeechRecognition ||
+  window.mozSpeechRecognition ||
+  window.msSpeechRecognition)();
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 3;
+
+startListeningButton.addEventListener('click', function() {
+  recognition.start();
+});
+
+recognition.onresult = function(event) {
+  handleStringOfSpeech(event.results[0][0].transcript);
+};
+
+function handleStringOfSpeech(str) {
+  var arr = str.split(' ');
+
+  for (var i = 0; arr.length > i; i++) {
+    var wordLowerCase = arr[i].toLowerCase();
+    if (colors.indexOf(wordLowerCase) >= 0) {
+      var index = colors.indexOf(wordLowerCase);
+      document.body.style.backgroundColor = colors[index];
+    }
+  }
+  updateColorText(colors[index]);
+}
+
+function updateColorText(color) {
+  colorText.textContent = color;
+}
